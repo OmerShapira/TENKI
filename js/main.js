df988dba843974ef75652ef3d02a8eac54f1c55b
Omer Shapira <oshapira@nvidia.com>
2017-07-01 22:17:53 -0400

    tidy

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-
-var textures 			= {}
-var shaders 			= {}
-var framebuffers 		= new Map() 
+var textures      = {}
+var shaders       = {}
+var framebuffers    = new Map()
 var framebufferTextures = new Map()
-var programs 			= new Map() 
+var programs      = new Map()
 
-var canvas = document.getElementById("mainCanvas")	
+var canvas = document.getElementById("mainCanvas")
 var gl = InitGLContext(canvas)
 
 var myText
@@ -20,159 +19,159 @@ if (gl) { Start(gl) }
 
 function Start(gl)
 {
-	LoadAssets(gl, textures)
-	LoadShaders(gl, shaders, new Map([
-		["vs-ScreenSpace", 	gl.VERTEX_SHADER],
-		["vs-TextBlock",	gl.VERTEX_SHADER],
-		["fs-Main", 		gl.FRAGMENT_SHADER],
-		["fs-FontSampler", 	gl.FRAGMENT_SHADER],
-		["fs-TextBlock", 	gl.FRAGMENT_SHADER]
-		]))
+  LoadAssets(gl, textures)
+  LoadShaders(gl, shaders, new Map([
+    ["vs-ScreenSpace",  gl.VERTEX_SHADER],
+    ["vs-TextBlock",	gl.VERTEX_SHADER],
+    ["fs-Main",     gl.FRAGMENT_SHADER],
+    ["fs-FontSampler",  gl.FRAGMENT_SHADER],
+    ["fs-TextBlock",  gl.FRAGMENT_SHADER]
+    ]))
 
-	programs.set("FontSampler", CreateProgram(gl, 
-			[shaders["vs-ScreenSpace"], shaders["fs-FontSampler"]]
-		))
-	programs.set("Main", CreateProgram(gl, 
-			[shaders["vs-ScreenSpace"], shaders["fs-Main"]]
-		))
-	programs.set("TextBlock", CreateProgram(gl,
-			[shaders["vs-TextBlock"], shaders["fs-TextBlock"]]
-		))
+  programs.set("FontSampler", CreateProgram(gl,
+      [shaders["vs-ScreenSpace"], shaders["fs-FontSampler"]]
+    ))
+  programs.set("Main", CreateProgram(gl,
+      [shaders["vs-ScreenSpace"], shaders["fs-Main"]]
+    ))
+  programs.set("TextBlock", CreateProgram(gl,
+      [shaders["vs-TextBlock"], shaders["fs-TextBlock"]]
+    ))
 
-	//Set up Buffers
-	var vertexBuffer = new Float32Array([
-		//locations
-		-1,		1,	
-		1,		1,	
-		-1,		-1,	
-		1,		-1,
-		//uvs
-		0,		0,
-		1,		0,
-		0,		1,
-		1,		1
-		])
+  //Set up Buffers
+  var vertexBuffer = new Float32Array([
+    //locations
+    -1,		1,
+    1,		1,
+    -1,		-1,
+    1,		-1,
+    //uvs
+    0,		0,
+    1,		0,
+    0,		1,
+    1,		1
+    ])
 
-	var indexBuffer = new Int16Array([0,1,2, 3,2,1])
-	
-	//Set up GL	buffers
-	screen_vertex = gl.createBuffer()
-	gl.bindBuffer(gl.ARRAY_BUFFER, screen_vertex)
-	gl.bufferData(gl.ARRAY_BUFFER, vertexBuffer, gl.STATIC_DRAW)
+  var indexBuffer = new Int16Array([0,1,2, 3,2,1])
 
-	screen_index = gl.createBuffer()
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, screen_index)
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexBuffer, gl.STATIC_DRAW)
-	
-	//Set up attributes	
-	for (let pgm of programs.values())
-	{
-		var positionLocation = gl.getAttribLocation(pgm, "vPosition")
-		gl.enableVertexAttribArray(positionLocation)
-		gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, gl.FALSE, 0, 0)
+  //Set up GL	buffers
+  screen_vertex = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, screen_vertex)
+  gl.bufferData(gl.ARRAY_BUFFER, vertexBuffer, gl.STATIC_DRAW)
 
-		var texCoordLocation = gl.getAttribLocation(pgm, "vTexCoord")
-		gl.enableVertexAttribArray(texCoordLocation)
-		gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, gl.FALSE, 0, 8 * 4)
-	}	
+  screen_index = gl.createBuffer()
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, screen_index)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexBuffer, gl.STATIC_DRAW)
 
-	//Reset Size
+  //Set up attributes
+  for (let pgm of programs.values())
+  {
+    var positionLocation = gl.getAttribLocation(pgm, "vPosition")
+    gl.enableVertexAttribArray(positionLocation)
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, gl.FALSE, 0, 0)
 
-	ResizeCanvas()
+    var texCoordLocation = gl.getAttribLocation(pgm, "vTexCoord")
+    gl.enableVertexAttribArray(texCoordLocation)
+    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, gl.FALSE, 0, 8 * 4)
+  }
 
-	//Set up uniforms
+  //Reset Size
 
-	WithProgram(programs.get("FontSampler"), (pgm) => {
-		gl.uniform1i(gl.getUniformLocation(pgm, "font"), 0)
-	})
+  ResizeCanvas()
 
-	WithProgram(programs.get("Main"), (pgm) => {
-		gl.uniform1i(gl.getUniformLocation(pgm, "mainTex"), 1)
-		gl.uniform1i(gl.getUniformLocation(pgm, "bg"),      2)
-		gl.uniform1f(gl.getUniformLocation(pgm, "image_presence"), 0) 
-	})
-	
+  //Set up uniforms
 
-	//Bind textures
-	
-	var tex = CreateTexture(gl, {
-		"width": gl.drawingBufferWidth,
-		"height": gl.drawingBufferHeight,
-		"min" : gl.LINEAR, 
-		"mag" : gl.LINEAR, 
-		"wrap": gl.CLAMP_TO_EDGE, 
-		"generateMipmap": false})
-	framebufferTextures.set("Text", tex)
-	var fb = gl.createFramebuffer()
-	framebuffers.set("Text", fb)
-	
-	WithFramebuffer (fb, () => {
-		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0)
-	})
+  WithProgram(programs.get("FontSampler"), (pgm) => {
+    gl.uniform1i(gl.getUniformLocation(pgm, "font"), 0)
+  })
 
-	//set up drawing context
-
-	gl.clearColor(0,0,0,1)
-	gl.enable(gl.DEPTH_TEST)
-	gl.depthFunc(gl.LEQUAL)
-	
-	///
+  WithProgram(programs.get("Main"), (pgm) => {
+    gl.uniform1i(gl.getUniformLocation(pgm, "mainTex"), 1)
+    gl.uniform1i(gl.getUniformLocation(pgm, "bg"),      2)
+    gl.uniform1f(gl.getUniformLocation(pgm, "image_presence"), 0)
+  })
 
 
-	myText = TextBlock(String.fromCharCode(0x26C4, 0x2B94)+"ＨＥＬＬＯ   Ｙｏｕｒ ｍｅｍｏｒｉｅｓ ａｒｅ ｓａｆｅ", 0.02, [0.1, 0.7])
+  //Bind textures
+
+  var tex = CreateTexture(gl, {
+    "width": gl.drawingBufferWidth,
+    "height": gl.drawingBufferHeight,
+    "min" : gl.LINEAR,
+    "mag" : gl.LINEAR,
+    "wrap": gl.CLAMP_TO_EDGE,
+    "generateMipmap": false})
+  framebufferTextures.set("Text", tex)
+  var fb = gl.createFramebuffer()
+  framebuffers.set("Text", fb)
+
+  WithFramebuffer (fb, () => {
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0)
+  })
+
+  //set up drawing context
+
+  gl.clearColor(0,0,0,1)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+
+  ///
 
 
-	//start
+  myText = TextBlock(String.fromCharCode(0x26C4, 0x2B94)+"ＨＥＬＬＯ   Ｙｏｕｒ ｍｅｍｏｒｉｅｓ ａｒｅ ｓａｆｅ", 0.02, [0.1, 0.7])
 
-	Loop(0) 
+
+  //start
+
+  Loop(0)
 }
 
 function Update(gl, time)
 {
-	Timer.Tick(time)
+  Timer.Tick(time)
 
-	for (let pgm of programs.values())
-	{
-		WithProgram(pgm, () => {
-			gl.uniform1f(gl.getUniformLocation(pgm, "time"), time)
-		})
-	}
-	
+  for (let pgm of programs.values())
+  {
+    WithProgram(pgm, () => {
+      gl.uniform1f(gl.getUniformLocation(pgm, "time"), time)
+    })
+  }
+
 }
 
 function Draw(gl, time){
 
 
-	BindTextureAt(textures["unifont"], gl.TEXTURE_2D, 0)
+  BindTextureAt(textures["unifont"], gl.TEXTURE_2D, 0)
 
-	WithFramebuffer(framebuffers.get("Text"), () => {
-		WithProgram(programs.get("FontSampler"), (pgm) => {
+  WithFramebuffer(framebuffers.get("Text"), () => {
+    WithProgram(programs.get("FontSampler"), (pgm) => {
 
-			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-			gl.bindBuffer(gl.ARRAY_BUFFER, screen_vertex)
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+      gl.bindBuffer(gl.ARRAY_BUFFER, screen_vertex)
 
-			let positionLocation = gl.getAttribLocation(pgm, "vPosition")
-			gl.enableVertexAttribArray(positionLocation)
-			gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, gl.FALSE, 0, 0)
+      let positionLocation = gl.getAttribLocation(pgm, "vPosition")
+      gl.enableVertexAttribArray(positionLocation)
+      gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, gl.FALSE, 0, 0)
 
-			let texCoordLocation = gl.getAttribLocation(pgm, "vTexCoord")
-			gl.enableVertexAttribArray(texCoordLocation)
-			gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, gl.FALSE, 0, 8 * 4)
-			
-			gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
+      let texCoordLocation = gl.getAttribLocation(pgm, "vTexCoord")
+      gl.enableVertexAttribArray(texCoordLocation)
+      gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, gl.FALSE, 0, 8 * 4)
 
-		})
+      gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
 
-		myText.Draw()
-	})
+    })
 
-	BindTextureAt(framebufferTextures.get("Text"), gl.TEXTURE_2D, 1)
-	BindTextureAt(textures["bg"], gl.TEXTURE_2D, 2)
+    myText.Draw()
+  })
 
-	WithProgram(programs.get("Main"), () => {
-		gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT)
-		gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
-	})
+  BindTextureAt(framebufferTextures.get("Text"), gl.TEXTURE_2D, 1)
+  BindTextureAt(textures["bg"], gl.TEXTURE_2D, 2)
+
+  WithProgram(programs.get("Main"), () => {
+    gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT)
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
+  })
 }
 
 
@@ -181,163 +180,163 @@ function Draw(gl, time){
 
 function Loop(time)
 {
-	window.requestAnimationFrame(Loop)
-	Update(gl, time)
-	Draw(gl, time)
+  window.requestAnimationFrame(Loop)
+  Update(gl, time)
+  Draw(gl, time)
 }
 
 function LoadTextureFromSource(name, source, params)
 {
-	return new Promise( (resolve, reject) => {
-		var img = new Image()
-		//TODO (OS): Make optional
-		img.crossOrigin = "anonymous"
-		img.onload = () => 
-		{ 
-			params.pixels = img
-			//TODO (OS): This blocks too, need to async
-			let tex = CreateTexture(gl, params)
-			if (tex) 
-			{
-				textures[name] = tex 
-				resolve(tex);
-			}
-			else 
-			{ 
-				reject ("Couldn't Create Texture : " + name)
-			}
-		}
-		img.src = source
-	})
+  return new Promise( (resolve, reject) => {
+    var img = new Image()
+    //TODO (OS): Make optional
+    img.crossOrigin = "anonymous"
+    img.onload = () =>
+    {
+      params.pixels = img
+      //TODO (OS): This blocks too, need to async
+      let tex = CreateTexture(gl, params)
+      if (tex)
+      {
+        textures[name] = tex
+        resolve(tex);
+      }
+      else
+      {
+        reject ("Couldn't Create Texture : " + name)
+      }
+    }
+    img.src = source
+  })
 }
 
 function LoadAssets(gl, textures)
 {
-	//TODO (OS): Make this cleaner and through the top level
-	LoadTextureFromSource("unifont", "assets/unifont-8.0.01.bmp", {"min" : gl.LINEAR, "mag": gl.NEAREST, "wrap": gl.REPEAT, "generateMipmap" : true}) 
+  //TODO (OS): Make this cleaner and through the top level
+  LoadTextureFromSource("unifont", "assets/unifont-8.0.01.bmp", {"min" : gl.LINEAR, "mag": gl.NEAREST, "wrap": gl.REPEAT, "generateMipmap" : true})
 }
 
 
 function CreateTexture(gl, params)
 {
-	params = params || {}
-	var tex = gl.createTexture()
-	gl.bindTexture(gl.TEXTURE_2D, tex)
-	if (params.width && params.height)
-	{
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, params.width, params.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, params["pixels"] || null)
-	}
-	else
-	{
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, params["pixels"] || null)
-	}
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, params["mag"] || gl.CLAMP)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, params["min"] ||gl.NEAREST_MIPMAP_NEAREST)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, params["wrap"] || gl.CLAMP_TO_EDGE)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, params["wrap"] || gl.CLAMP_TO_EDGE)
-	if (params["generateMipmap"]) { gl.generateMipmap(gl.TEXTURE_2D) }
-	gl.bindTexture(gl.TEXTURE_2D, null)
-	return tex
+  params = params || {}
+  var tex = gl.createTexture()
+  gl.bindTexture(gl.TEXTURE_2D, tex)
+  if (params.width && params.height)
+  {
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, params.width, params.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, params["pixels"] || null)
+  }
+  else
+  {
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, params["pixels"] || null)
+  }
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, params["mag"] || gl.CLAMP)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, params["min"] ||gl.NEAREST_MIPMAP_NEAREST)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, params["wrap"] || gl.CLAMP_TO_EDGE)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, params["wrap"] || gl.CLAMP_TO_EDGE)
+  if (params["generateMipmap"]) { gl.generateMipmap(gl.TEXTURE_2D) }
+  gl.bindTexture(gl.TEXTURE_2D, null)
+  return tex
 }
 
 function CreateProgram(gl, shaders)
 {
-	var pgm = gl.createProgram()
+  var pgm = gl.createProgram()
 
-	for (let shader of shaders) { gl.attachShader(pgm, shader) }
+  for (let shader of shaders) { gl.attachShader(pgm, shader) }
 
-	gl.linkProgram(pgm)
+  gl.linkProgram(pgm)
 
-	if (!gl.getProgramParameter(pgm, gl.LINK_STATUS))
-	{
-		var info = gl.getProgramInfoLog(pgm)
-		console.error("Could not link program : " + info)
-		return null	
-	}
+  if (!gl.getProgramParameter(pgm, gl.LINK_STATUS))
+  {
+    var info = gl.getProgramInfoLog(pgm)
+    console.error("Could not link program : " + info)
+    return null
+  }
 
-	return pgm
+  return pgm
 }
 
 function LoadShaders(gl, shaders, list)
 {
-	let create = (src, type) => 
-	{
-		var shader = gl.createShader(type)
-		gl.shaderSource(shader, src)
-		gl.compileShader(shader)
+  let create = (src, type) =>
+  {
+    var shader = gl.createShader(type)
+    gl.shaderSource(shader, src)
+    gl.compileShader(shader)
 
-		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-		{
-			var info = gl.getShaderInfoLog(shader)
-			console.error("Shader Compile Error: " + info)
-		}
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+    {
+      var info = gl.getShaderInfoLog(shader)
+      console.error("Shader Compile Error: " + info)
+    }
     return shader
-	}
+  }
 
-	let source = (id) => document.getElementById(id).innerHTML
+  let source = (id) => document.getElementById(id).innerHTML
 
-	let store = (id, type) => { shaders[id] = create(source(id), type) }
+  let store = (id, type) => { shaders[id] = create(source(id), type) }
 
-	for (let [key, val] of list)
-	{
-		store(key, val)
-	}
-	
+  for (let [key, val] of list)
+  {
+    store(key, val)
+  }
+
 }
 
 function BindTextureAt(texture, type, bindOffset)
 {
-	let active = gl.getParameter(gl.ACTIVE_TEXTURE)
-	gl.activeTexture(gl.TEXTURE0 + bindOffset)
-	gl.bindTexture(type, texture)
-	gl.activeTexture(active)
+  let active = gl.getParameter(gl.ACTIVE_TEXTURE)
+  gl.activeTexture(gl.TEXTURE0 + bindOffset)
+  gl.bindTexture(type, texture)
+  gl.activeTexture(active)
 }
 
 function UpdateViewportSize(gl)
 {
-	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
-	for (let pgm of programs.values())
-	{
-		gl.useProgram(pgm)
-		gl.uniform2f(gl.getUniformLocation(pgm, "screenResolution"), gl.drawingBufferWidth, gl.drawingBufferHeight);
-	}
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+  for (let pgm of programs.values())
+  {
+    gl.useProgram(pgm)
+    gl.uniform2f(gl.getUniformLocation(pgm, "screenResolution"), gl.drawingBufferWidth, gl.drawingBufferHeight);
+  }
 }
 
 function WithProgram(pgm, func)
 {
-	let active = gl.getParameter(gl.CURRENT_PROGRAM)
-	gl.useProgram(pgm)
-	func(pgm)
-	gl.useProgram(active)
+  let active = gl.getParameter(gl.CURRENT_PROGRAM)
+  gl.useProgram(pgm)
+  func(pgm)
+  gl.useProgram(active)
 }
 
 function WithFramebuffer(fb, func)
 {
-	gl.bindFramebuffer(gl.FRAMEBUFFER, fb)
-	func(fb)
-	gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fb)
+  func(fb)
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null)
 }
 
 function InitGLContext(canvas)
 {
-	try
-	{
-		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
-	}
-	catch(e) {}
-	let supported = gl && gl.getExtension('ANGLE_instanced_arrays')
+  try
+  {
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
+  }
+  catch(e) {}
+  let supported = gl && gl.getExtension('ANGLE_instanced_arrays')
 
-	if (!supported) 
-	{
-		console.error("WebGL is not enabled on this browser")
-		gl = null
-	}
+  if (!supported)
+  {
+    console.error("WebGL is not enabled on this browser")
+    gl = null
+  }
   return gl
 }
 
 function ResizeCanvas ()
 {
-	canvas.width 	= window.innerWidth
-	canvas.height 	= window.innerHeight
-	if (gl){ UpdateViewportSize(gl) }
+  canvas.width  = window.innerWidth
+  canvas.height   = window.innerHeight
+  if (gl){ UpdateViewportSize(gl) }
 }
--- js/text.js
+++ js/text.js
@@ -1,83 +1,83 @@
 function TextBlock (text, size, position)
 {
-	const ANGLE = gl.getExtension('ANGLE_instanced_arrays')
+  const ANGLE = gl.getExtension('ANGLE_instanced_arrays')
 
-	let _text = text || ""
-	let _size = size || 0.05
-	let _position = position || [0.5, 0.5]
+  let _text = text || ""
+  let _size = size || 0.05
+  let _position = position || [0.5, 0.5]
 
-	let _instanceData = [] 
-	
-	var _instanceDataBuffer
-	var _charCount =  0
+  let _instanceData = []
 
-	//make display list
-	for (let i = 0; i < _text.length; ++i)
-	{
-		//TODO (OS): find a better way to detect whitespace
-		if (_text[i] == _text[i].trim())
-		{
-			//calculate position
-			let offset = _getOffset(i)
-			_instanceData.push(_position[0] + offset[0], position[1] + offset[1])
+  var _instanceDataBuffer
+  var _charCount =  0
 
-			//calculate unicode
-			let uv = _getTextUV_UL(_text[i])
-			_instanceData.push(uv[0], uv[1])
+  //make display list
+  for (let i = 0; i < _text.length; ++i)
+  {
+    //TODO (OS): find a better way to detect whitespace
+    if (_text[i] == _text[i].trim())
+    {
+      //calculate position
+      let offset = _getOffset(i)
+      _instanceData.push(_position[0] + offset[0], position[1] + offset[1])
 
-			++_charCount
-		}
-	}
+      //calculate unicode
+      let uv = _getTextUV_UL(_text[i])
+      _instanceData.push(uv[0], uv[1])
 
-	
-	function _getOffset (index)
-	{
-		return [_position[0] + _size * 2.0 * index, _position[1]]
-		//TODO (OS): implement wrap
-	}
+      ++_charCount
+    }
+  }
 
-	function _getTextUV_UL (character)
-	{
-		let code = character.charCodeAt(0)
-		return [(code & 0x00FF) / 256.0, ((code & 0xFF00) >> 8) / 256.0]
-	}
 
-	//setup
-	
-	WithProgram (programs.get("TextBlock"), (pgm) => {
-		_instanceDataBuffer = gl.createBuffer()
-		gl.bindBuffer(gl.ARRAY_BUFFER, _instanceDataBuffer)
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(_instanceData), gl.STATIC_DRAW)		
-	})
+  function _getOffset (index)
+  {
+    return [_position[0] + _size * 2.0 * index, _position[1]]
+    //TODO (OS): implement wrap
+  }
 
-	return {
-		Draw: function()
-		{
-			WithProgram(programs.get("TextBlock"), (pgm)=>{
-				let len = Math.round(_charCount)
+  function _getTextUV_UL (character)
+  {
+    let code = character.charCodeAt(0)
+    return [(code & 0x00FF) / 256.0, ((code & 0xFF00) >> 8) / 256.0]
+  }
 
-				
-				gl.bindBuffer(gl.ARRAY_BUFFER, _instanceDataBuffer)
+  //setup
 
-				let sizeLocation = gl.getUniformLocation(pgm, "size")
-				gl.uniform1f(sizeLocation, _size)
-				
-				let offsetLocation = gl.getAttribLocation(pgm, "offset")
-				gl.enableVertexAttribArray(offsetLocation)
-				gl.vertexAttribPointer(offsetLocation, 2, gl.FLOAT, gl.FALSE, 4 * 4, 0)
-				ANGLE.vertexAttribDivisorANGLE(offsetLocation, 1) 
+  WithProgram (programs.get("TextBlock"), (pgm) => {
+    _instanceDataBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, _instanceDataBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(_instanceData), gl.STATIC_DRAW)
+  })
 
-				let charUVLocation = gl.getAttribLocation(pgm, "charUV")
-				gl.enableVertexAttribArray(charUVLocation)
-				gl.vertexAttribPointer(charUVLocation, 2, gl.FLOAT, gl.FALSE, 4 * 4, 2 * 4)
-				ANGLE.vertexAttribDivisorANGLE(charUVLocation, 1)
+  return {
+    Draw: function()
+    {
+      WithProgram(programs.get("TextBlock"), (pgm)=>{
+        let len = Math.round(_charCount)
 
-				ANGLE.drawElementsInstancedANGLE(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0, len)
 
-				// ANGLE.vertexAttribDivisorANGLE(offsetLocation, 0) 
-				// ANGLE.vertexAttribDivisorANGLE(charUVLocation, 0)
-			
-			})
-		}
-	}
+        gl.bindBuffer(gl.ARRAY_BUFFER, _instanceDataBuffer)
+
+        let sizeLocation = gl.getUniformLocation(pgm, "size")
+        gl.uniform1f(sizeLocation, _size)
+
+        let offsetLocation = gl.getAttribLocation(pgm, "offset")
+        gl.enableVertexAttribArray(offsetLocation)
+        gl.vertexAttribPointer(offsetLocation, 2, gl.FLOAT, gl.FALSE, 4 * 4, 0)
+        ANGLE.vertexAttribDivisorANGLE(offsetLocation, 1)
+
+        let charUVLocation = gl.getAttribLocation(pgm, "charUV")
+        gl.enableVertexAttribArray(charUVLocation)
+        gl.vertexAttribPointer(charUVLocation, 2, gl.FLOAT, gl.FALSE, 4 * 4, 2 * 4)
+        ANGLE.vertexAttribDivisorANGLE(charUVLocation, 1)
+
+        ANGLE.drawElementsInstancedANGLE(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0, len)
+
+        // ANGLE.vertexAttribDivisorANGLE(offsetLocation, 0)
+        // ANGLE.vertexAttribDivisorANGLE(charUVLocation, 0)
+
+      })
+    }
+  }
 }
