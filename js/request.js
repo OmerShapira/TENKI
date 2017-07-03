a3f34aae19a9ddbf9f0a717dc6affae23bb41e37
Omer Shapira <oshapira@nvidia.com>
2017-07-02 23:02:17 -0400

    tidy

--- js/request.js
+++ js/request.js
@@ -1,124 +1,122 @@
-
 function RequestURL(url)
 {
-	return new Promise ((resolve, reject) =>
-	{
-		var request = new XMLHttpRequest()
-		request.open("GET", url)
+  return new Promise ((resolve, reject) =>
+  {
+    var request = new XMLHttpRequest()
+    request.open("GET", url)
 
-		request.onload = () => 
-		{
-			if (request.status === 200)
-			{
-				resolve(request.response)
-			}
-			else
-			{
-				reject(Error(request.statusText))
-			}
-		}
+    request.onload = () =>
+    {
+      if (request.status === 200)
+      {
+        resolve(request.response)
+      }
+      else
+      {
+        reject(Error(request.statusText))
+      }
+    }
 
-		request.onerror = () => {reject(Error("Network Error"))}
+    request.onerror = () => {reject(Error("Network Error"))}
 
-		request.send()
-	})
+    request.send()
+  })
 }
 
 function RequestLocationFromRemoteService()
 {
-	return RequestURL("https://location.services.mozilla.com/v1/geolocate?key=test")
+  return RequestURL("https://location.services.mozilla.com/v1/geolocate?key=test")
 }
 
 function RequestLocationNameFromRemoteService(lat, lon)
 {
-	let url = "http://nominatim.openstreetmap.org/reverse?format=json&lat=" + lat + "&lon=" + lon + "&zoom=18&addressdetails=1"
-	
-	return RequestURL(url)	
+  let url = "http://nominatim.openstreetmap.org/reverse?format=json&lat=" + lat + "&lon=" + lon + "&zoom=18&addressdetails=1"
+
+  return RequestURL(url)
 }
 
 function RequestLocationFromBrowser ()
 {
-	return new Promise( (resolve, reject) =>
-	{
-		if ("geolocation" in navigator)
-		{
-			navigator.geolocation.getCurrentPosition(resolve)
-		}
-		else
-	 	{
-	 		reject(Error("No Geolocation"))
-	 	}
-	}
-	)	
+  return new Promise( (resolve, reject) =>
+  {
+    if ("geolocation" in navigator)
+    {
+      navigator.geolocation.getCurrentPosition(resolve)
+    }
+    else
+    {
+      reject(Error("No Geolocation"))
+    }
+  }
+  )
 }
 
 //TODO (OS): Convert everything to work this way
 var Flickr  = {
 
-	// https://www.flickr.com/services/api/flickr.places.findByLatLon.html
-	BASE_URL: "https://api.flickr.com/services/rest/?",
+  // https://www.flickr.com/services/api/flickr.places.findByLatLon.html
+  BASE_URL: "https://api.flickr.com/services/rest/?",
 
-	RequestSearchByLatLong : function(lat, lng)
-	{
-		// let method = "flickr.places.findByLatLon"
-		let method = "flickr.photos.search"
-		let accuracy = 11 //dunno, let's try that?
-		let sort = "interestingness-desc"
-		let content_type = 1
-		let geo_context = 2 //outdoors
-		
-		let url = Flickr.BASE_URL + "&method=" + method + "&lat=" + lat + "&lon=" + lng + "&accuracy=" + accuracy + "&sort=" + sort + "&content_type=" + content_type + "&privacy_filter=1" + Flickr.EPILOG
-		return RequestURL(url).then(Flickr.StripResponse)
-	},
+  RequestSearchByLatLong : function(lat, lng)
+  {
+    // let method = "flickr.places.findByLatLon"
+    let method = "flickr.photos.search"
+    let accuracy = 11 //dunno, let's try that?
+    let sort = "interestingness-desc"
+    let content_type = 1
+    let geo_context = 2 //outdoors
 
-	RequestGetInfo : function (id)
-	{
-		let method = "flickr.photos.getInfo"
-		let url = Flickr.BASE_URL + "&method=" + method + "&photo_id=" + id + Flickr.EPILOG
-		return RequestURL(url).then(Flickr.StripResponse)
-	},
+    let url = Flickr.BASE_URL + "&method=" + method + "&lat=" + lat + "&lon=" + lng + "&accuracy=" + accuracy + "&sort=" + sort + "&content_type=" + content_type + "&privacy_filter=1" + Flickr.EPILOG
+    return RequestURL(url).then(Flickr.StripResponse)
+  },
 
-	RequestGetSizes : function (id)
-	{
-		let method = "flickr.photos.getSizes"
-		let url = Flickr.BASE_URL + "&method=" + method + "&photo_id=" + id + Flickr.EPILOG
-		return RequestURL(url).then(Flickr.StripResponse)
-	},
+  RequestGetInfo : function (id)
+  {
+    let method = "flickr.photos.getInfo"
+    let url = Flickr.BASE_URL + "&method=" + method + "&photo_id=" + id + Flickr.EPILOG
+    return RequestURL(url).then(Flickr.StripResponse)
+  },
 
-	ConstructPhotoURL : function (id, size, server, farm, secret)
-	{
-		/* 
-		from https://www.flickr.com/services/api/misc.urls.html:
+  RequestGetSizes : function (id)
+  {
+    let method = "flickr.photos.getSizes"
+    let url = Flickr.BASE_URL + "&method=" + method + "&photo_id=" + id + Flickr.EPILOG
+    return RequestURL(url).then(Flickr.StripResponse)
+  },
 
-		https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-			or
-		https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
-			or
-		https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
-		*/
+  ConstructPhotoURL : function (id, size, server, farm, secret)
+  {
+    /*
+    from https://www.flickr.com/services/api/misc.urls.html:
 
-		return "https://farm" + farm + ".staticflickr.com/" + server + "/" +  id + "_" + secret + "_" + size + ".jpg"
-	},
+    https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+      or
+    https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+      or
+    https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
+    */
 
-	StripResponse : function(response)
-	{
-		return new Promise((resolve, reject) =>
-	{	
-			let initial = "jsonFlickrApi("
-			if (response.length < initial.length + 1) 
-			{
-				reject(Error("illegal response"))
-			}
-			else if (response.substring(0, initial.length) === initial)
-			{
-				resolve(response.substring(initial.length, response.length - 1))
-			}
-			else
-			{
-				reject(Error("illegal response : " + response))
-			}
-		})
-		
-	},
+    return "https://farm" + farm + ".staticflickr.com/" + server + "/" +  id + "_" + secret + "_" + size + ".jpg"
+  },
+
+  StripResponse : function(response)
+  {
+    return new Promise((resolve, reject) =>
+  {
+      let initial = "jsonFlickrApi("
+      if (response.length < initial.length + 1)
+      {
+        reject(Error("illegal response"))
+      }
+      else if (response.substring(0, initial.length) === initial)
+      {
+        resolve(response.substring(initial.length, response.length - 1))
+      }
+      else
+      {
+        reject(Error("illegal response : " + response))
+      }
+    })
+
+  },
 }
-
--- js/timer.js
+++ js/timer.js
@@ -1,56 +1,55 @@
 Timer = (()=>{
 
-	let _subscribers = new Set()
-	let _time = 0 
+  let _subscribers = new Set()
+  let _time = 0
 
-	let time = () =>
-	{
-		if (_time <= 0) time = performance.now()
-		return _time
-	}
+  let time = () =>
+  {
+    if (_time <= 0) time = performance.now()
+    return _time
+  }
 
-	function _count(duration, update)
-	{
-		return new Promise( (resolve, reject) => 
-		{
-			// let _t = time()
-			//HACK (OS): Dunno why this doesn't work
-			let _t = _time
-			let _begin = _time
-			let end = _t + duration 
-			let handle = _tick
+  function _count(duration, update)
+  {
+    return new Promise( (resolve, reject) =>
+    {
+      // let _t = time()
+      //HACK (OS): Dunno why this doesn't work
+      let _t = _time
+      let _begin = _time
+      let end = _t + duration
+      let handle = _tick
 
-			function _tick(t)
-			{
-				//relative, absolute, delta, remain,
-				if (update)
-				{
-					update(t - _begin, t - _t, end - t)	
-				}
-				
-				_t = t
+      function _tick(t)
+      {
+        //relative, absolute, delta, remain,
+        if (update)
+        {
+          update(t - _begin, t - _t, end - t)
+        }
 
-				if (t >= end) 
-				{ 
-					_subscribers.delete(handle)
-					resolve(t)
-				}
-			}
-			_subscribers.add(handle)
-		})
-	}
+        _t = t
 
-	return {
-		Tick : (t) =>
-		{
-			_time = t
-			for (let sub of _subscribers)
-			{
-				sub(_time)
-			}
-		},
-		Count : _count
-	}
-	
+        if (t >= end)
+        {
+          _subscribers.delete(handle)
+          resolve(t)
+        }
+      }
+      _subscribers.add(handle)
+    })
+  }
+
+  return {
+    Tick : (t) =>
+    {
+      _time = t
+      for (let sub of _subscribers)
+      {
+        sub(_time)
+      }
+    },
+    Count : _count
+  }
+
 })()
-
